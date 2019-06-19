f009e955f28c628b9f07330cac12f5555593bf40
Igor Souza <igor.souza@wunderman.com>
2019-06-19 17:18:46 -0300

    Adiciona função para calcular o tempo do post

--- src/App.js
+++ src/App.js
@@ -14,19 +14,19 @@ class App extends Component {
       post: [
         {
           'name': 'Diego Schell Fernandes',
-          'date': '18-06-2019',
+          'date': '2019/06/19 14:00:00',
           'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis animi dolorem, repudiandae obcaecati repellendus fuga laboriosam corrupti voluptatibus quibusdam accusamus eligendi at voluptatum consequuntur ad dolore hic et quidem accusantium?',
           'picture': 'https://picsum.photos/id/781/200/200'
         },
         {
           'name': 'Rubia Savolksi',
-          'date': '18-06-2019',
+          'date': '2019/06/11 14:00:00',
           'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. obcaecati repellendus fuga laboriosam corrupti voluptatibus quibusdam accusamus eligendi at voluptatum consequuntur ad dolore hic et quidem accusantium? Lorem ipsum dolor sit amet consectetur adipisicing elit. obcaecati repellendus fuga laboriosam corrupti voluptatibus quibusdam accusamus eligendi at voluptatum consequuntur ad dolore hic et quidem accusantium?',
           'picture': 'https://picsum.photos/id/447/200/200'
         },
         {
           'name': 'Mr. Mustache',
-          'date': '18-06-2019',
+          'date': '2019/05/15 14:00:00',
           'content': 'Lorem ipsum dolor. Blanditiis animi dolorem, repudiandae obcaecati repellendus fuga laboriosam corrupti voluptatibus quibusdam accusamus ad dolore hic et quidem accusantium?',
           'picture': 'https://picsum.photos/id/815/200/200'
         }
--- src/components/PostHeader/index.js
+++ src/components/PostHeader/index.js
@@ -3,13 +3,34 @@ import React, { Component } from 'react';
 import { Container, Avatar, Name } from './styles';
 
 class PostHeader extends Component {
+  timeConversion = (date) => {
+    const postDate = new Date(date);
+    const dateNow = new Date();
+    const millisec = Math.abs(postDate - dateNow);
+
+    const seconds = Math.round((millisec / 1000));
+    const minutes = Math.round((millisec / (1000 * 60)));
+    const hours = Math.round((millisec / (1000 * 60 * 60)));
+    const days = Math.round((millisec / (1000 * 60 * 60 * 24)));
+
+    if (seconds < 60) {
+      return seconds + " segundos";
+    } else if (minutes < 60) {
+      return minutes + " minutos";
+    } else if (hours < 24) {
+      return hours + " horas";
+    } else {
+      return days + " dias"
+    }
+  }
+
   render() {
     return (
       <Container>
         <Avatar background={this.props.picture} />
         <Name>
           <h1>{this.props.name}</h1>
-          <span>há {this.props.time}</span>
+          <span>há {this.timeConversion(this.props.time)}</span>
         </Name>
       </Container>
     );
